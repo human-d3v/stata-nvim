c36ead9812cdfbb2ee93beb814406728f3367a0f
human-d3v <brytop05@gmail.com>
2025-01-15 14:45:41 -0700

    Added autocmd and augroup

--- lua/init.lua
+++ lua/init.lua
@@ -1,10 +1,20 @@
 local M = {}
+local augroup = vim.api.nvim_create_augroup("Stata", {clear = true})
 
-function M.setup(opts)
+function M.main(opts)
 	local terminal = require("terminal")
 	local repl = require("repl")
 	local opts = opts or {stata_license_type = "stata-mp"}
 	require("lsp").setup()
+end
+
+function M.setup(opts)
+	vim.api.nvim_create_autocmd("VimEnter", 
+		{group = augroup, 
+		pattern = {"stata"},
+		desc = "Stata LSP Setup",
+		once = true, 
+		callback = M.main(opts)})
 end
 
 return M
