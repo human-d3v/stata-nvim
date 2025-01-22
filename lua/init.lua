34caa1b2335cc11ba89436a87e69efb57df5a29e
human-d3v <brytop05@gmail.com>
2025-01-22 11:43:29 -0700

    Starting over fml

--- lua/init.lua
+++ lua/init.lua
@@ -1,28 +1,30 @@
-local augroup = vim.api.nvim_create_augroup("Stata", {clear = true})
+-- local augroup = vim.api.nvim_create_augroup("Stata", {clear = true})
+--
+-- local function main() --opts)
+-- 	-- local terminal = require("terminal")
+-- 	-- local repl = require("repl")
+-- 	-- local opts = opts or {stata_license_type = "stata-mp"}
+-- 	require("lsp").setup()
+-- end
+--
+-- local function setup()--opts)
+-- 	vim.api.nvim_create_autocmd("VimEnter", 
+-- 		{group = augroup, 
+-- 		pattern = {"stata"},
+-- 		desc = "Stata LSP Setup",
+-- 		once = true, 
+-- 		-- callback = main()--opts)})
+-- 		callback = function ()
+-- 			vim.api.nvim_create_autocmd('FileType', {
+-- 				pattern = 'stata',
+-- 				callback = function ()
+-- 					print("starting LSP for Stata")
+-- 				end
+-- 			})
+-- 		end
+-- 	})
+-- end
+--
+-- setup()
 
-local function main() --opts)
-	-- local terminal = require("terminal")
-	-- local repl = require("repl")
-	-- local opts = opts or {stata_license_type = "stata-mp"}
-	require("lsp").setup()
-end
-
-local function setup()--opts)
-	vim.api.nvim_create_autocmd("VimEnter", 
-		{group = augroup, 
-		pattern = {"stata"},
-		desc = "Stata LSP Setup",
-		once = true, 
-		-- callback = main()--opts)})
-		callback = function ()
-			vim.api.nvim_create_autocmd('FileType', {
-				pattern = 'stata',
-				callback = function ()
-					print("starting LSP for Stata")
-				end
-			})
-		end
-	})
-end
-
-setup()
+print("HERE AM I")
