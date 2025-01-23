32da9f77e2c1642889212126b1702c8ff83f9c9e
human-d3v <brytop05@gmail.com>
2025-01-23 15:47:04 -0700

    Used plenary to build lsp binary

--- plugin/stata_lsp.lua
+++ plugin/stata_lsp.lua
@@ -1,1 +1,34 @@
+local Job = require('plenary.job')
 
+local compile = function()
+  Job:new({
+    command = "bun",
+    args = {"build", "./server/src/server.ts", "--compile", "--outfile", "server_bin"},
+  })
+end
+
+local install = function ()
+	Job:new({
+		command = "npm",
+    args = {"install"},
+		on_exit = function() compile() end
+	})
+end
+
+local init = function ()
+	Job:new({
+		command = "npm",
+		args = {"init", "-y"},
+		on_exit = function() install() end
+	})
+end
+
+local build_binary_server = function()
+	Job:new({
+		command = "cd",
+		args = {"lsp-server"},
+		on_exit = function() init() end
+	})
+end
+
+build_binary_server()
